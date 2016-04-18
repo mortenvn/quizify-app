3d6819138b7d4b0c3a4d46d450fccbc0e2668d4f
Morten Noddeland <morten@noddeland.no>
2016-04-18 14:24:22 +0200

    Added simple registration view

--- app/src/main/java/net/quizifyapp/quizifyapp/MainActivity.java
+++ app/src/main/java/net/quizifyapp/quizifyapp/MainActivity.java
@@ -3,8 +3,18 @@ package net.quizifyapp.quizifyapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+    Button registerButton;
+    EditText usernameField;
+    EditText emailField;
+    EditText passwordField;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,17 +22,28 @@ public class MainActivity extends AppCompatActivity {
         NetworkManager.getInstance(this);
         setContentView(R.layout.activity_main);
 
+        registerButton = (Button) findViewById(R.id.register_button);
+        usernameField = (EditText) findViewById(R.id.username);
+        emailField = (EditText) findViewById(R.id.email);
+        passwordField = (EditText) findViewById(R.id.password);
 
-        // Example request
-        NetworkManager.getInstance().register("morten@qandroid.net", "qan", "123qwe123", new APIListener<String>() {
+    }
+
+    public void setLogin(View v) {
+        String usernameText = usernameField.getText().toString();
+        String emailText = emailField.getText().toString();
+        String passwordText = passwordField.getText().toString();
+
+        NetworkManager.getInstance().register(emailText, usernameText, passwordText, new APIListener<String>() {
             @Override
             public void getResult(String result) {
                 if (!result.isEmpty()) {
-                    Log.d("FUCYEAH", result);
+                    Log.d("Fuck yeah TOKEN", result);
+                    Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
                     // TODO: Handle errors
-                    //do what you need with the result...
                 }
             }
         });
+
     }
 }
